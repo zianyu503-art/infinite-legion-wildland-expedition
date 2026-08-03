3fab5733a20a4fc2b4bca40a1a5b528b8bfa302a
zianyu503-art <zianyu503@gmail.com>
2026-08-03 12:27:51 +0800

    Update final PWA build and save compatibility

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785678513|5315807';
+const CACHE_VERSION = '1785731190|3058252';
 /** @type {string} */
 const CACHE_PREFIX = 'Infinite Legion:-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
