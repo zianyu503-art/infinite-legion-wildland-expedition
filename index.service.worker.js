d9283b4aef7bf8fc5023f1a5b4e8a3fd598798a9
zianyu503-art <zianyu503@gmail.com>
2026-08-03 19:39:18 +0800

    Add Heavy Cannon live-fire verification

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785755574|3381654';
+const CACHE_VERSION = '1785757068|5599310';
 /** @type {string} */
 const CACHE_PREFIX = 'Infinite Legion:-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
