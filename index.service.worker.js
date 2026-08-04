eded3e6a206127e3157ca3a6fb32f75e17de15bf
zianyu503-art <zianyu503@gmail.com>
2026-08-04 19:56:52 +0800

    Publish Kaeron chaos finale and creator credit

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785837199|3119546';
+const CACHE_VERSION = '1785844492|2341862';
 /** @type {string} */
 const CACHE_PREFIX = 'Infinite Legion:-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
