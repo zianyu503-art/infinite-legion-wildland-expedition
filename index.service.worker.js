df5e5022ddb26d25091e6cb635f17e9e86a0e59c
zianyu503-art <zianyu503@gmail.com>
2026-08-05 12:56:29 +0800

    Add Aionis postgame world boss

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785844492|2341862';
+const CACHE_VERSION = '1785905607|2888793';
 /** @type {string} */
 const CACHE_PREFIX = 'Infinite Legion:-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
