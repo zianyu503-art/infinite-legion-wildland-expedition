61ee6a5e60863c5a67f1565e374c40799904da74
zianyu503-art <zianyu503@gmail.com>
2026-08-05 13:33:28 +0800

    Make Aionis map beacon permanently visible

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785905607|2888793';
+const CACHE_VERSION = '1785907745|2110085';
 /** @type {string} */
 const CACHE_PREFIX = 'Infinite Legion:-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
